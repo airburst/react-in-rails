a074365cfbc6b771335df4748047af75bbc11c3b
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2023-10-20 12:15:51 +0100

    feat: Add Mobius

---
+++ app/javascript/src/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,31 @@
+import React, { useState, FC } from "react";
+import { Divider, Text, TextField } from "@simplybusiness/mobius";
+import "@simplybusiness/theme-sb";
+
+type HelloProps = {
+  name: string;
+};
+
+const HelloWorld: FC<HelloProps> = ({ name }: HelloProps) => {
+  const [_name, setName] = useState(name);
+
+  const handleChange = (value: string) => {
+    setName(value);
+  };
+
+  return (
+    <div>
+      <Text variant="h1">Hello, {_name}!</Text>
+      <Divider />
+      <form>
+        <TextField
+          label="Say hello to:"
+          value={_name}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default HelloWorld;
