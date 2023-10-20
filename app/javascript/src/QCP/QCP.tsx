6670da8e7af57e7ac7d54a324a6b5b5a0b00ea32
Mark Fairhurst <mark.fairhurst@simplybusiness.co.uk>
2023-10-20 16:44:48 +0100

    fix: All tests passing

--- app/javascript/src/QCP/QCP.tsx
+++ app/javascript/src/QCP/QCP.tsx
@@ -2,6 +2,7 @@ import { ErrorBoundary } from "@components/ErrorBoundary";
 import { Providers } from "@components/Providers";
 import { QuoteComparisonPage } from "@components/QuoteComparisonPage";
 import { ThemeContext } from "@simplybusiness/mobius";
+import "@simplybusiness/theme-sb";
 import React, { useContext, useEffect } from "react";
 import { breakpoints } from "./constants";
 import { RFQ } from "./types";
